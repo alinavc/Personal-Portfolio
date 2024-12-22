457222d871660e2dd0439c43951ad5510f9d5d5e
Alina <78280131+alinavc@users.noreply.github.com>
2024-12-22 15:49:00 -0800

    add dark/light mode icons for future buttons

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -1,3 +1,15 @@
 export const Home = () =>{
-    return <div> Home </div>;
+    return(
+        <div className="entirePage"> Home 
+        <section id='home'>
+            <h1>Hello I'm <b>Alina Corpora</b>.</h1>
+            <h2>I am a senior Computer Science student with current experience in web development, particularly using <b>MERN</b>.</h2>
+            <a href="https://github.com/alinavc" className='githubImg'></a>
+        </section>
+        <section id='about'></section>
+        <section id='projects'></section>
+        <section id='skills'></section>
+        <section id= 'experience'></section>
+        <section id='contact'></section>
+        </div>);
 };
