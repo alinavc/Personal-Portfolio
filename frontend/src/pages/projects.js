f7020c43a82fea38763eec8277556b53fe92b6a0
Alina <78280131+alinavc@users.noreply.github.com>
2025-01-15 20:59:27 -0800

    connected projects api to frontend

--- frontend/src/pages/projects.js
+++ frontend/src/pages/projects.js
@@ -1,9 +1,29 @@
 import '../App.css';
+import {useEffect,useState} from 'react';
+import axios from 'axios';
 
 export const Projects = () => {
+
+    const[projects, setProj] = useState([]);
+
+    useEffect(() => {
+        const fetchProj = async() =>{
+            try{
+                const response = await axios.get("http://localhost:4000/");
+                setProj(response.data);
+                console.log(response.data);
+            } catch(err){
+                console.log(err);
+            }
+        };
+
+        fetchProj();
+    },[]);
+
     return(
         <div>
             <section className='section-header'>Projects</section>
+            <ul></ul>
         </div>
     );
 }
