b519f678bc799a0696bbbb5a983c19af79de7023
Alina <78280131+alinavc@users.noreply.github.com>
2025-01-18 15:45:43 -0800

    Began Experiences section, imported more icons

--- frontend/src/pages/experience.js
+++ frontend/src/pages/experience.js
@@ -1,9 +1,28 @@
 import '../App.css';
+import './experience.css';
 
 export const Experience = () => {
     return(
         <div>
             <section className='section-header'>Experience</section>
+            <div className='exp-timeline'>
+                <hr className='exp-line' width='5px' size='1000'/>
+                <div className='exp-events'>
+                    <div id='tl1'>
+                        <h3>American River College</h3>
+                        <h4>Sacramento, CA</h4>
+                        <p></p>
+                        <a className='gradIcon'></a>
+                        <p>2018-2023</p>
+                    </div>
+                    <div id='tl2'>
+                        <a className='gradIcon'></a>
+                    </div>
+                    <div id='tl3'>
+                        <a className='jobIcon'></a>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
