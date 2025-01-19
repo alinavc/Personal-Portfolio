414a6e3ca5dda2113bb7bac6b429ae77e989fa57
Alina <78280131+alinavc@users.noreply.github.com>
2025-01-18 23:14:12 -0800

    fixed resume button

--- frontend/src/pages/experience.js
+++ frontend/src/pages/experience.js
@@ -11,15 +11,29 @@ export const Experience = () => {
                     <div id='tl1'>
                         <h3>American River College</h3>
                         <h4>Sacramento, CA</h4>
-                        <p></p>
+                        <p>I graduated with an <b>Associate's degree</b> in <b>Computer Science</b>,
+                        <b>Physical Science/ Mathematics</b>, and <b>Social Sciences</b> in 2022.</p>
                         <a className='gradIcon'></a>
-                        <p>2018-2023</p>
+                        <p>2018 - 2022</p>
                     </div>
                     <div id='tl2'>
+                        <h3>California State University, Sacramento</h3>
+                        <h4>Sacramento, CA</h4>
+                        <p>I will graduate with a <b>Bachelor's degree</b> in <b>Computer Science</b>, as well
+                            as a <b>Certificate in Game Engineering</b> in <b>May 2026</b>.
+                        </p>
                         <a className='gradIcon'></a>
+                        <p>2023 - Present</p>
                     </div>
                     <div id='tl3'>
+                        <h3>Full-Stack Developer</h3>
+                        <h4>Sacramento, CA</h4>
+                        <p>I am now working as a freelancer full-stack developer through Upwork. My main
+                            stack includes MongoDB, Express, React, and Node.js. I especially enjoy coding in
+                            C++ and Java.
+                        </p>
                         <a className='jobIcon'></a>
+                        <p>2025 - Present</p>
                     </div>
                 </div>
             </div>
--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -1,6 +1,7 @@
 import './home.css';
 import '../App.css';
 import alinaImg from "../assets/icons/alina.jpg";
+import myResume from '../assets/files/resume.pdf';
 
 export const Home = () => {
     return(
@@ -11,7 +12,7 @@ export const Home = () => {
                 <h2>I am a senior Computer Science student with current experience in web development, particularly using <b>MERN</b>.</h2>
                 <div className='homeLinks'>
                     <a href="#contact" className='home-button' id='nonIconButton'>Contact</a>
-                    <a href="./test.txt" download className='home-button' id='nonIconButton'>My Resume</a>
+                    <a href={myResume} download='AlinaCorpora_Resume.pdf' className='home-button' id='nonIconButton'>My Resume</a>
                     <a href="https://github.com/alinavc" target="_blank" rel='noreferrer' id='githubImg'
                       className='home-button' title="To GitHub">GitHub</a>
                     <a href="https://www.linkedin.com/in/alina-corpora-10a3b1308/" target="_blank" rel='noreferrer' title="To LinkedIn"
