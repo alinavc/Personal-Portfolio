0486385cdab71c2314de0e4198c814b90e71535f
Alina <78280131+alinavc@users.noreply.github.com>
2025-01-19 23:44:36 -0800

    Begin contact form

--- frontend/src/pages/contact.js
+++ frontend/src/pages/contact.js
@@ -1,9 +1,18 @@
 import '../App.css';
+import './contact.css';
 
 export const Contact = () => {
     return(
         <div>
             <section className='section-header'>Contact</section>
+            <div className='contactBox'>
+                <form>
+                    <input type='text' id='name' placeholder='Your Name' required></input>
+                    <input type='email' id='email' placeholder='Your Email' required></input>
+                    <textarea id='msg' placeholder='Send me a message' required></textarea>
+                    <button type='submit'>Send</button>
+                </form>
+            </div>
         </div>
     );
 };
