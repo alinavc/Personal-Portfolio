e1d48be67a30b318ad2e4d367de4733f3d8b36b2
Alina <78280131+alinavc@users.noreply.github.com>
2025-01-21 15:29:22 -0800

    finished contact form

--- frontend/src/pages/contact.js
+++ frontend/src/pages/contact.js
@@ -1,8 +1,8 @@
+import { useEffect, useState } from 'react';
 import '../App.css';
 import './contact.css';
 
 export const Contact = () => {
-
     return(
         <div>
             <section className='section-header'>Contact</section>
@@ -11,10 +11,11 @@ export const Contact = () => {
                         <b><a href='mailto:alinacorpora@gmail.com'>alinacorpora@gmail.com</a></b>
                         or fill out the form below and I will get back to you. Thank you!
                     </p>
-                <form className='contactBox' onSubmit={sendEmail}>
-                    <input type='text' id='name' className='contact-input' placeholder='Your Name' required></input>
-                    <input type='email' id='email' className='contact-input' placeholder='Your Email' required></input>
-                    <textarea id='msg' className='contact-input' placeholder='Send me a message' required></textarea>
+                <form className='contactBox' action="https://api.web3forms.com/submit" method="POST">
+                    <input type="hidden" name="access_key" value={process.env.REACT_APP_CONTACT_KEY}/>
+                    <input type='text' name='name' placeholder='Your Name' required></input>
+                    <input type='email' name='email' placeholder='Your Email' required></input>
+                    <textarea name='message' placeholder='Send me a message' required></textarea>
                     <button type='submit'>Send</button>
                 </form>
             </div>
--- frontend/src/siteLayout.js
+++ frontend/src/siteLayout.js
@@ -6,8 +6,16 @@ import {Projects} from './pages/projects.js';
 import {Experience} from './pages/experience.js';
 import {Contact} from './pages/contact.js';
 import {ARR} from './pages/arr.js';
+import moonLogo from './assets/icons/siteMode/moon.png';
+import sunLogo from './assets/icons/siteMode/sun.png';
 
 export const Layout = () =>{
+    var modeIcon = moonLogo;
+    let getTheme=JSON.parse(localStorage.getItem('appMode'));
+    if(getTheme==="DARK"){
+        document.body.classList='darkMode';
+        modeIcon=sunLogo;
+    }
 
     return(
         <div>
@@ -29,9 +37,25 @@ export const Layout = () =>{
         <section id='contact'>
             <Contact/>
         </section>
-        <a className='modeSwitch'></a>
+        <img src={modeIcon} id='modeSwitch' style={{width:'50px',height:'50px'}} title='Dark Mode' onClick={checkMode}></img>
         <section>
             <ARR/>
         </section>
         </div>);
 };
+
+const checkMode=()=>{
+    var mode;
+    var icon=document.getElementById('modeSwitch');
+    document.body.classList.toggle('darkMode');
+    if(document.body.classList.contains('darkMode')){
+        icon.src=sunLogo;
+        icon.title="Light Mode";
+        mode='DARK';
+    } else{
+        icon.src=moonLogo;
+        icon.title='Dark Mode';
+        mode='LIGHT'
+    }
+    localStorage.setItem('appMode',JSON.stringify(mode));
+}
