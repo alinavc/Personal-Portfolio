7a6d192be895b24215d186d98186e45752a708bd
Alina <78280131+alinavc@users.noreply.github.com>
2025-01-21 22:50:43 -0800

    delete vercel

--- frontend/src/pages/arr.js
+++ frontend/src/pages/arr.js
@@ -5,7 +5,7 @@ export const ARR = () => {
     return(
         <div className='arrBox'>
             <p>&copy; 2025 Alina Corpora. All rights reserved.</p>
-            <p><b>About this website: </b> Made using MongoDB, Express, React, Node.js, as well as Vercel hosting.</p>
+            <p><b>About this website: </b> Made using MongoDB, Express, React, Node.js, as well as Hostinger hosting.</p>
         </div>
     );
 }
